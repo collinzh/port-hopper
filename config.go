49dba77b3e0e3a5710f09242d0f759a2e55616da
Collin Zhang <czhang@eyereturn.com>
2019-05-10 17:04:55 -0400

    Getting started

---
+++ config.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"strings"
+)
+
+type Address struct {
+	Host string
+	Port int
+}
+
+type Config struct {
+	BindAddresses []Address
+	Destination   Address
+}
+
+var config *Config
+
+func GetConfiguration() *Config {
+	if config != nil {
+		return config
+	}
+
+	bindHostAddr := flag.String("bind", "0.0.0.0", "Bind hosts. Example: 192.168.0.10")
+	bindPortAddr := flag.Int("port", 0, "Bind port. Example: 8080")
+	destAddr := flag.String("dest", "", "Destination address. Example: 10.0.0.2:8080")
+
+	flag.Parse()
+
+	// parse bind hosts
+	hosts := strings.Split(*bindHostAddr, ",")
+	bindHosts := make([]string, 0)
+	for _, host := range hosts {
+		host = strings.TrimSpace(host)
+		if len(host) == 0 {
+			continue
+		}
+		bindHosts = append(bindHosts, host)
+	}
+
+	if len(bindHosts) == 0 {
+		log.Panicln("no hosts to bind")
+	}
+
+	// parse bind port
+	if *bindPortAddr <= 0 || *bindPortAddr > 65535 {
+		log.Panicf("invalid port number %d", *bindPortAddr)
+	}
+
+	bindAddr := make([]Address, len(hosts))
+	for idx, host := range hosts {
+		bindAddr[idx] = Address{Host: host, Port: *bindPortAddr}
+	}
+
+	config = &Config{
+		BindAddresses: bindAddr,
+	}
+}
